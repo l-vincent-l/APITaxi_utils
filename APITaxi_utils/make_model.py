f0a6a605b88aa8966560f704e22fd8da5f0bf2e0
l-vincent-l <lara.vincent@gmail.com>
2016-02-04 16:39:17 +0100

    remove old hack

--- APITaxi_utils/make_model.py
+++ APITaxi_utils/make_model.py
@@ -9,14 +9,7 @@ def make_model(filename, model_name, api, *args, **kwargs):
     list_names.extend([str(i) for i in args])
     list_names.extend([k + "_" + str(v) for k, v in kwargs.items()])
     register_name = "_".join(list_names)
-    #@TODO: gros hack degueu, il faudra que cette fonction soit une méthode de classe des models
     details_dict = model.marshall_obj(*args, **kwargs)
-    if 'operateur_id' in details_dict:
-        del details_dict['operateur_id']
-        details_dict['operateur'] = basefields.String(attribute='operateur.email')
-    if 'departement_id' in details_dict:
-        del details_dict['departement_id']
-        details_dict['departement'] = basefields.String(attribute='departement.numero')
     details = api.model(register_name + "_details", details_dict)
     return api.model(register_name,
         {"data": basefields.List(basefields.Nested(details))})
