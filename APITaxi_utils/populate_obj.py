2dac0a6af10344df77a202b8dee8448d5c07f3d2
l-vincent-l <lara.vincent@gmail.com>
2016-02-05 11:45:13 +0100

    Add get_short_uuid

--- APITaxi_utils/populate_obj.py
+++ APITaxi_utils/populate_obj.py
@@ -4,6 +4,7 @@ from . import HistoryMixin
 def create_obj_from_json(cls, json_obj):
     keys = [k for k in cls.__table__.columns if k.name not in HistoryMixin.to_exclude()]
     required_keys = [k.name for k in keys if not k.nullable and not k.primary_key]
+    print keys, required_keys
     for key in required_keys:
         if key not in json_obj or json_obj[key] is None:
             raise KeyError(key)
