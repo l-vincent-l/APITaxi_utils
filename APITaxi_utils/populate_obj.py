624642191e3fb0e0d52c0d4af9e604968d4123f3
l-vincent-l <lara.vincent@gmail.com>
2016-02-12 11:23:59 +0100

    add obj to create_obj_from_json

--- APITaxi_utils/populate_obj.py
+++ APITaxi_utils/populate_obj.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 from .mixins import HistoryMixin
 
-def create_obj_from_json(cls, json_obj):
+def create_obj_from_json(cls, json_obj, obj=None):
     keys = [k for k in cls.__table__.columns if k.name not in HistoryMixin.to_exclude()]
     required_keys = [k.name for k in keys if not k.nullable and not k.primary_key]
     for key in required_keys:
         if key not in json_obj or json_obj[key] is None:
             raise KeyError(key)
-    new_obj = cls()
+    if obj is None:
+        obj = cls()
     for k in keys:
         name = k.name
         if name not in json_obj:
             continue
-        setattr(new_obj, name, json_obj[name])
-    return new_obj
+        setattr(obj, name, json_obj[name])
+    return obj
