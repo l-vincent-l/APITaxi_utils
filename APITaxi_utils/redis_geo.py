1798bce63b61ffc665606b4010c27f7fce6f8946
l-vincent-l <lara.vincent@gmail.com>
2016-06-08 10:17:12 +0200

    Add storedistkey in georadius

--- APITaxi_utils/redis_geo.py
+++ APITaxi_utils/redis_geo.py
@@ -10,12 +10,13 @@ class GeoRedis(StrictRedis):
     def georadius(self, geoset, lat, lon, radius=15, units='km',
             withdistance=True, withcoordinates=True, withhash=False,
             withgeojson=False, withgeojsoncollection=False,
-            noproperties=False, order='asc', version=3.2):
+            noproperties=False, order='asc', storekey=None, storedistkey=None,
+            version=3.2):
         command = 'georadius {geoset} {lat} {lon} {radius} {units}'.format(
                 geoset=geoset, lat=lat, lon=lon, radius=radius, units=units)
-        if withdistance:
+        if withdistance and not storekey and not storedistkey:
             command += ' withdist' if version == 3.2 else ' withdistance'
-        if withcoordinates:
+        if withcoordinates and not storekey and not storedistkey:
             command += ' withcoord' if version == 3.2 else ' withcoordinates'
         if withhash:
             command += ' withhash'
@@ -26,14 +27,26 @@ class GeoRedis(StrictRedis):
         if noproperties:
             command += ' noproperties'
         command += ' '+order
+        if storekey and version == 3.2:
+            command += ' store {}'.format(storekey)
+        if storedistkey and version == 3.2:
+            command += ' storedist {}'.format(storedistkey)
+        from flask import current_app
         try:
-            return self.execute_command(command)
+            if version == 3.2 or (not storekey and not storedistkey):
+                return self.execute_command(command)
+            else:
+                r = self.execute_command(command)
+                if len(r) == 0:
+                    return []
+                return self.zadd(storekey or storedistkey,
+                            **{id_:d for id_, d, _ in r})
         except ResponseError as e:
             if version != 3.2:
                 raise e
             self.georadius(geoset, lat, lon, radius, units, withdistance,
                 withcoordinates, withhash, withgeojson, withgeojsoncollection,
-                noproperties, order, 2.8)
+                noproperties, order, storekey, storedistkey, 2.8)
 
     def geopos(self, key, *members):
         try:
