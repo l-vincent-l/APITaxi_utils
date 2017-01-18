325d4a532d5948c52492489a2d8aa96a9a386177
l-vincent-l <lara.vincent@gmail.com>
2017-01-18 16:04:24 +0100

    Add Mixin ResourceFileOrJSON

---
+++ APITaxi_utils/resource_file_or_json.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from flask_restplus import Resource, abort
+from flask import request
+from flask_security import current_user
+from APITaxi_utils.request_wants_json import request_wants_json
+from datetime import datetime
+from APITaxi_utils.slack import slack as slacker
+
+
+class ResourceFileOrJSON(Resource):
+
+    def post(self):
+        if 'file' in request.files:
+            return self.post_file()
+        if request_wants_json():
+            return self.post_json()
+        else:
+            abort(400, message="Unable to find file")
+
+
+    def post_file(self):
+        filename = "{}-{}-{}.csv".format(self.filetype, current_user.email,
+                str(datetime.now()))
+        self.documents.save(request.files['file'], name=filename)
+        slack = slacker()
+        if slack:
+            slack.chat.post_message('#taxis',
+                'Un nouveau fichier {} a été envoyé par {}. {}'.format(
+                    self.filetype, current_user.email,
+                    url_for('documents.documents', filename=filename, _external=True)
+                )
+            )
+        return "OK"
