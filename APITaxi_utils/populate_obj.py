28b4710c8711f654458e5541a0290dba33273ea8
l-vincent-l <lara.vincent@gmail.com>
2017-01-30 16:09:17 +0100

    Simplify create_obj_from_json

--- APITaxi_utils/populate_obj.py
+++ APITaxi_utils/populate_obj.py
@@ -2,16 +2,23 @@
 from .mixins import HistoryMixin
 
 def create_obj_from_json(cls, json_obj, obj=None):
-    keys = [k for k in cls.__table__.columns if k.name not in HistoryMixin.to_exclude()]
-    required_keys = [k.name for k in keys if not k.nullable and not k.primary_key]
+    cols = [k for k in cls.__table__.columns
+            if k.name not in HistoryMixin.to_exclude()
+    ]
+    if hasattr(cls, '_additionnal_columns'):
+        cols += cls._additionnal_columns
+    required_keys = [c.name for c in cols
+                     if not c.nullable and not c.primary_key\
+                     and (not hasattr(c, 'default') or c.default is None)
+    ]
     for key in required_keys:
         if key not in json_obj or json_obj[key] is None:
             raise KeyError("Missing key: '{}'".format(key))
-    if obj is None:
-        obj = cls()
-    for k in keys:
-        name = k.name
-        if name not in json_obj:
-            continue
-        setattr(obj, name, json_obj[name])
-    return obj
+
+    keys = [k.name for k in cols]
+    if hasattr(cls, '_additionnal_keys'):
+        keys += cls._additionnal_keys
+    return cls(**{k: json_obj[k] for k in map(lambda k: unicode(k), keys)
+                  if k in json_obj
+                 }
+              )
